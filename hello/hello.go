d411d5b0eb8d3bccd2a40854d60687bed264616e
bogdan <bogdandemchenko@gmail.com>
2019-08-21 09:22:42 +0300

    french test

--- hello/hello.go
+++ hello/hello.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 
 	if language == spanish {
